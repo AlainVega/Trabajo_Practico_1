85bb9ba81ed077fff246c6ff1afce6ebeb5fc9dc
Alain Vega <alainjosevz@gmail.com>
2020-09-16 19:21:10 -0400

    Archivo punto h y punto c

---
+++ src/Antenas.h
@@ -0,0 +1,18 @@
+/*
+ * Antenas.h
+ *
+ *  Created on: 16 sept. 2020
+ *      Author: lp1-2019
+ */
+
+//#ifndef ANTENAS_H_
+//#define ANTENAS_H_
+
+
+#define TAM_CIUDAD 10
+#define N_ANTENAS 2
+
+int antenas[N_ANTENAS][4] = { {0, 0, 1, 4}, {1, 4, 0, 4} };
+
+
+// #endif /* ANTENAS_H_ */
